1da7d2ced03cac9fefa3449452a029dfafe05e05
barbequeue <p99kab00@gmail.com>
2019-01-14 22:54:42 +0200

    Vuetify plugin added; Static layout on Home page added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="home">
-    <h1>todo_app</h1>
-  </div>
+  <v-container py-0>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>todo_app</h1>
+        <p class="mb-0">Simply manage your tasks</p>
+      </v-flex>
+    </v-layout>
+    <v-layout collumn wrap>
+      <v-flex xs12 my-3>
+        <v-card dark>
+          <v-card-text>Task 1</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 my-3>
+        <v-card dark>
+          <v-card-text>Task 2</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 my-3>
+        <v-card dark>
+          <v-card-text>Task 3</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
