b513ff2c7a9f0fa1b3644765abc7b574429a4459
barbequeue <p99kab00@gmail.com>
2019-01-14 21:48:01 +0200

    Started blank project; Added favicon and custom font

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,700')
+
+html, body
+  margin: 0
+  box-sizing: border-box
+  font-family: 'Motserrat', sans-serif
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>todo_app</h1>
+  </div>
+</template>
