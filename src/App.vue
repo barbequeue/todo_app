707f59379831a814f53312e4d8b221c7c8ec3523
p99kab00 <p99kab00@gmail.com>
2019-01-20 01:28:52 +0200

    Default layout changed; Added theme switcher

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app :dark="themeIsDark" :light="!themeIsDark">
+    <v-toolbar app>
+      <v-toolbar-title>todo_app</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon>fas fa-sun</v-icon>
+      <v-switch @change="this.SWITCH_THEME" class="shrink pt-0 ml-3 mr-2" color="white" :hideDetails=true></v-switch>
+      <v-icon>fas fa-moon</v-icon>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="sass">
@@ -12,3 +21,20 @@ html, body
   box-sizing: border-box
   font-family: 'Motserrat', sans-serif
 </style>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState([
+      'themeIsDark'
+    ])
+  },
+  methods: {
+    ...mapMutations([
+      'SWITCH_THEME'
+    ])
+  }
+}
+</script>
